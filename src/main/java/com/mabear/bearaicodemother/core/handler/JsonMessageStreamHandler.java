b3c82982b6f3a303e0af49a95d4dc99d15fef6a9
码熊 <2992374334@qq.com>
2025-08-29 11:42:46 +0800

    十 - 系统优化（实时性优化） 1. Vue 工程异步构建改为同步构建，构建完成后才结束 SSE 连接

--- src/main/java/com/mabear/bearaicodemother/core/handler/JsonMessageStreamHandler.java
+++ src/main/java/com/mabear/bearaicodemother/core/handler/JsonMessageStreamHandler.java
@@ -8,7 +8,6 @@ import com.mabear.bearaicodemother.ai.model.message.StreamMessage;
 import com.mabear.bearaicodemother.ai.model.message.ToolExecutedMessage;
 import com.mabear.bearaicodemother.ai.model.message.ToolRequestMessage;
 import com.mabear.bearaicodemother.ai.tools.BaseTool;
-import com.mabear.bearaicodemother.constant.AppConstant;
 import com.mabear.bearaicodemother.core.builder.VueProjectBuilder;
 import com.mabear.bearaicodemother.manager.ToolManager;
 import com.mabear.bearaicodemother.model.entity.User;
@@ -63,9 +62,9 @@ public class JsonMessageStreamHandler {
                     // 流式响应完成后，添加 AI 消息到对话历史
                     String aiResponse = chatHistoryStringBuilder.toString();
                     chatHistoryService.addChatMessage(appId, aiResponse, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
-                    // 异步构造 Vue 项目
-                    String projectPath = AppConstant.CODE_OUTPUT_ROOT_DIR + "/vue_project_" + appId;
-                    vueProjectBuilder.buildProjectAsync(projectPath);
+//                    // 异步构造 Vue 项目
+//                    String projectPath = AppConstant.CODE_OUTPUT_ROOT_DIR + "/vue_project_" + appId;
+//                    vueProjectBuilder.buildProjectAsync(projectPath);
                 })
                 .doOnError(error -> {
                     // 如果AI回复失败，也要记录错误消息
