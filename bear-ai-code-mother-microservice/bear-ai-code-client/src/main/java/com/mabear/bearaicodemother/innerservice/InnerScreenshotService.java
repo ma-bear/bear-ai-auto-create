16a89d3fd14900399a2fa61832d09630892c9c04
码熊 <2992374334@qq.com>
2025-09-01 19:33:39 +0800

    十二 - 微服务改造 - 服务接口模块（这次提交为中间态，影响了单体项目代码）

---
+++ bear-ai-code-mother-microservice/bear-ai-code-client/src/main/java/com/mabear/bearaicodemother/innerservice/InnerScreenshotService.java
@@ -0,0 +1,12 @@
+package com.mabear.bearaicodemother.innerservice;
+
+/**
+ *
+ * @author: ma bear
+ * @date: 2025/9/1 19:30
+ */
+public interface InnerScreenshotService {
+
+    String generateAndUploadScreenshot(String webUrl);
+
+}
---
+++ bear-ai-code-mother-microservice/bear-ai-code-client/src/main/java/com/mabear/bearaicodemother/innerservice/InnerUserService.java
@@ -0,0 +1,32 @@
+package com.mabear.bearaicodemother.innerservice;
+
+import com.mabear.bearaicodemother.exception.BusinessException;
+import com.mabear.bearaicodemother.exception.ErrorCode;
+import com.mabear.bearaicodemother.model.entity.User;
+import com.mabear.bearaicodemother.model.vo.UserVO;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import static com.mabear.bearaicodemother.constant.UserConstant.USER_LOGIN_STATE;
+
+public interface InnerUserService {
+
+    List<User> listByIds(Collection<? extends Serializable> ids);
+
+    User getById(Serializable id);
+
+    UserVO getUserVO(User user);
+
+    // 静态方法，避免跨服务调用
+    static User getLoginUser(HttpServletRequest request) {
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User currentUser = (User) userObj;
+        if (currentUser == null || currentUser.getId() == null) {
+            throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
+        }
+        return currentUser;
+    }
+}
