6b7c4f999c0de2c3b787680d073bf5ba02c3e66a
码熊 <2992374334@qq.com>
2025-09-01 11:48:25 +0800

    十一 - 可观测性 1. 接入 ARMS 2. 接入 Prometheus 自定义业务指标收集器（提供配置） 3. 接入 Grafana 自定义业务指标展示（提供配置）

--- src/main/java/com/mabear/bearaicodemother/service/impl/AppServiceImpl.java
+++ src/main/java/com/mabear/bearaicodemother/service/impl/AppServiceImpl.java
@@ -24,6 +24,8 @@ import com.mabear.bearaicodemother.model.enums.ChatHistoryMessageTypeEnum;
 import com.mabear.bearaicodemother.model.enums.CodeGenTypeEnum;
 import com.mabear.bearaicodemother.model.vo.AppVO;
 import com.mabear.bearaicodemother.model.vo.UserVO;
+import com.mabear.bearaicodemother.monitor.MonitorContext;
+import com.mabear.bearaicodemother.monitor.MonitorContextHolder;
 import com.mabear.bearaicodemother.service.AppService;
 import com.mabear.bearaicodemother.service.ChatHistoryService;
 import com.mabear.bearaicodemother.service.ScreenshotService;
@@ -115,10 +117,21 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
         }
         // 5. 通过校验后，添加用户消息到对话历史
         chatHistoryService.addChatMessage(appId, message, ChatHistoryMessageTypeEnum.USER.getValue(), loginUser.getId());
-        // 6. 调用 AI 生成代码（流式）
+        // 6. 设置监控上下文
+        MonitorContextHolder.setContext(
+                MonitorContext.builder()
+                        .userId(loginUser.getId().toString())
+                        .appId(appId.toString())
+                        .build()
+        );
+        // 7. 调用 AI 生成代码（流式）
         Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream(message, codeGenTypeEnum, appId);
-        // 7. 收集 AI 响应内容并在完成后记录到对话历史
-        return streamHandlerExecutor.doExecute(codeStream, chatHistoryService, appId, loginUser, codeGenTypeEnum);
+        // 8. 收集 AI 响应内容并在完成后记录到对话历史
+        return streamHandlerExecutor.doExecute(codeStream, chatHistoryService, appId, loginUser, codeGenTypeEnum)
+                .doFinally(signalType -> {
+                    // 流结束时清理（无论成功/失败/取消）
+                    MonitorContextHolder.clearContext();
+                });
     }
 
     @Override
